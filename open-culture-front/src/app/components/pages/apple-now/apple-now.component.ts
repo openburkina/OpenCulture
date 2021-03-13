4cc73884df6198a5bd8dc9f89f84347663e5b7ab
Abdül Nikiema <kiemaniabdul@gmail.com>
2021-03-13 00:30:34 +0000

    Initial commit

---
+++ open-culture-front/src/app/components/pages/apple-now/apple-now.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-apple-now',
+  templateUrl: './apple-now.component.html',
+  styleUrls: ['./apple-now.component.scss']
+})
+export class AppleNowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ open-culture-front/src/assets/js/custom.js
@@ -0,0 +1,113 @@
+(function($) {
+    'use strict';
+
+    // Mean Menu JS
+    $('.mean-menu').meanmenu({ 
+        meanScreenWidth: "991"
+    });
+
+    // Navbar Area
+    $(window).on('scroll', function() {
+        if ($(this).scrollTop() >150){  
+            $('.navbar-area').addClass("sticky-nav");
+        }
+        else{
+            $('.navbar-area').removeClass("sticky-nav");
+        }
+    });
+
+    // Search Popup JS
+    $('.close-btn').on('click',function() {
+        $('.search-overlay').fadeOut();
+        $('.search-btn').show();
+        $('.close-btn').removeClass('active');
+    });
+    $('.search-btn').on('click',function() {
+        $(this).hide();
+        $('.search-overlay').fadeIn();
+        $('.close-btn').addClass('active');
+    });
+
+    // Sidebar Modal JS
+	$(".burger-menu").on('click',  function() {
+		$('.sidebar-modal').toggleClass('active');
+	});
+	$(".sidebar-modal-close-btn").on('click',  function() {
+		$('.sidebar-modal').removeClass('active');
+    });
+
+    // Others Option For Responsive JS
+	$(".side-nav-responsive .dot-menu").on("click", function(){
+		$(".side-nav-responsive .container .container").toggleClass("active");
+    });
+
+    // Client Slider 
+    $('.client-slider').owlCarousel({
+        loop: true,
+        margin: 30,
+        nav: false,
+        dots: true,
+        autoplay: true,
+        autoplayHoverPause: true,
+        responsive:{
+            0:{
+                items: 1
+            },
+            1000:{
+                items: 2
+            }
+        }
+    })
+
+    // Tabs Single Page
+    $('.tab ul.tabs').addClass('active').find('> li:eq(0)').addClass('current');
+    $('.tab ul.tabs li a').on('click', function (g) {
+        var tab = $(this).closest('.tab'), 
+        index = $(this).closest('li').index();
+        tab.find('ul.tabs > li').removeClass('current');
+        $(this).closest('li').addClass('current');
+        tab.find('.tab_content').find('div.tabs_item').not('div.tabs_item:eq(' + index + ')').slideUp();
+        tab.find('.tab_content').find('div.tabs_item:eq(' + index + ')').slideDown();
+        g.preventDefault();
+    });
+
+    // FAQ Accordion JS
+	$('.accordion').find('.accordion-title').on('click', function(){
+		// Adds Active Class
+		$(this).toggleClass('active');
+		// Expand or Collapse This Panel
+		$(this).next().slideToggle('fast');
+		// Hide The Other Panels
+		$('.accordion-content').not($(this).next()).slideUp('fast');
+		// Removes Active Class From Other Titles
+		$('.accordion-title').not($(this)).removeClass('active');		
+    });
+
+    // Popup Video
+    $('.popup-btn').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false
+    });
+
+    // Back To Top Js
+    $('body').append('<div id="toTop" class="top-btn"><i class="bx bx-chevrons-up"></i></div>');
+    $(window).on('scroll',function () {
+        if ($(this).scrollTop() != 0) {
+            $('#toTop').fadeIn();
+        } else {
+            $('#toTop').fadeOut();
+        }
+    }); 
+    $('#toTop').on('click',function(){
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+
+    // WOW JS
+    new WOW().init();
+	
+})(jQuery);
