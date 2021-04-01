43d3167ff484660a2d8ac168704bcf13c64d564f
Abdoul Nikiema <sapienskiemani@gmail.com>
2021-04-01 19:04:10 +0100

    |_revu du css et ajout d'autres element au menu

--- open-culture-front/src/environments/environment.ts
+++ open-culture-front/src/environments/environment.ts
@@ -3,15 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-<<<<<<< HEAD
-  SERVER_URL : 'http://localhost:8080/api',
-  production: false
-=======
-  production: false,
-  apiUrl: 'http://localhost:8080/api/'
->>>>>>> 4f785ddddd2f3c094f7760542c3f22630eaf1e6e
-};
-
+    production: false,
+    apiUrl: 'http://localhost:8080/api/',
+    SERVER_URL: 'http://localhost:8080/api/',
+}
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
