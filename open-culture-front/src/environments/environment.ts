464aecac15962114e70bdfc799cd5192f7aca0f2
franck <franck.de.sales@gmail.com>
2021-07-15 19:41:18 +0000

    Meilleure config de CORS

--- open-culture-front/src/environments/environment.ts
+++ open-culture-front/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
     production: false,
-    apiUrl : 'http://localhost:8080/api/'
+    apiUrl : 'http://be:8080/api/'
 };
 /*
  * For easier debugging in development mode, you can import the following file
